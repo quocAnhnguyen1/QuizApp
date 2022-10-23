72944d4562f37453d0ff29228d0b516a51b55fec
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2022-10-23 20:34:22 +0200

    init

---
+++ src/test/java/de/htwberlin/webtech/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
